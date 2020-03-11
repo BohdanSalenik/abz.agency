f061528f3e08f23c9613a557ea020de6234429d1
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-03-11 12:31:19 +0200

    prep Template

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+export class App extends Component {
+
+  render() {
+    const { movies } = this.state;
+
+    return (
+      <div className="page">
+      </div>
+    );
+  }
+}
